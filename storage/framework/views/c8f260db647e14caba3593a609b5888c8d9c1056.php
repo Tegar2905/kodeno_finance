3f1ac4a0af53f773bda692848228f83bc752cd59
Tegar2905 <92562637+Tegar2905@users.noreply.github.com>
2021-12-05 17:27:35 +0700

    Add files via upload

---
+++ storage/framework/views/c8f260db647e14caba3593a609b5888c8d9c1056.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kodeno Finance | Invoice Out</title>
+    <link rel="stylesheet" type="text/css" href="asset/css/stylenav.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="asset/css/inistyle.css">
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+</head>
+<body>
+    
+
+<div id="konten">
+
+  <div class="container-xxl">
+
+        
+    <div class="container-lg">
+
+<div class="judul">
+<h1>Invoice</h1>
+<p>Untuk 1 tahun terakhir</p>
+</div>
+
+<div class="row">
+  <div class="col" style="text-align: right;">
+    <div class="btn-group">
+  <button type="button" class="btn btn-success dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    New Invoice
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="newinvoiceI.php">Item Base</a></li>
+    <li><a class="dropdown-item" href="newinvoiceW.php">Word Base</a></li>
+  </ul>
+</div>
+      <button type="button" class="btn btn-primary" style="width: 70px; margin-left: 15px;">
+        <i class="bi bi-printer"></i>
+      </button>
+
+    </div>
+</div>
+
+<br>
+
+  <div class="row">
+    <div class="col">
+      <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit"><i class="bi bi-search"></i></button>
+    </form>
+    </div>
+  </div>
+
+
+<!-- table -->
+  <br>
+
+<table class="table table-bordered">
+  <thead>
+    <tr class="table-dark">
+      <th scope="col">No. Invoice</th>
+      <th scope="col">Project Name</th>
+      <th scope="col">Invoice Date</th>
+      <th scope="col">Cost in IDR</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">XXX</th>
+      <td>XXX</td>
+      <td>XXX</td>
+      <td>Rp. 1.000.000</td>
+      <td width="130px"><button type="button" class="btn btn-outline-success" style="margin-right: 15px;"><i class="bi bi-pencil-square"></i></button>
+          <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">XXX</th>
+      <td>XXX</td>
+      <td>XXX</td>
+      <td>Rp. 1.000.000</td>
+      <td width="130px"><button type="button" class="btn btn-outline-success" style="margin-right: 15px;"><i class="bi bi-pencil-square"></i></button>
+          <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">XXX</th>
+      <td>XXX</td>
+      <td>XXX</td>
+      <td>Rp. 1.000.000</td>
+      <td width="130px"><button type="button" class="btn btn-outline-success" style="margin-right: 15px;"><i class="bi bi-pencil-square"></i></button>
+          <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">XXX</th>
+      <td>XXX</td>
+      <td>XXX</td>
+      <td>Rp. 1.000.000</td>
+      <td width="130px"><button type="button" class="btn btn-outline-success" style="margin-right: 15px;"><i class="bi bi-pencil-square"></i></button>
+          <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">XXX</th>
+      <td>XXX</td>
+      <td>XXX</td>
+      <td>Rp. 1.000.000</td>
+      <td width="130px"><button type="button" class="btn btn-outline-success" style="margin-right: 15px;"><i class="bi bi-pencil-square"></i></button>
+          <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">XXX</th>
+      <td>XXX</td>
+      <td>XXX</td>
+      <td>Rp. 1.000.000</td>
+      <td width="130px"><button type="button" class="btn btn-outline-success" style="margin-right: 15px;"><i class="bi bi-pencil-square"></i></button>
+          <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">XXX</th>
+      <td>XXX</td>
+      <td>XXX</td>
+      <td>Rp. 2.000.000</td>
+      <td width="130px"><button type="button" class="btn btn-outline-success" style="margin-right: 15px;"><i class="bi bi-pencil-square"></i></button>
+          <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">XXX</th>
+      <td>XXX</td>
+      <td>XXX</td>
+      <td>Rp. 2.500.000</td>
+      <td width="130px"><button type="button" class="btn btn-outline-success" style="margin-right: 15px;"><i class="bi bi-pencil-square"></i></button>
+          <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
+      </td>
+    </tr>
+    </tr>
+    <tr>
+      <th scope="row">XXX</th>
+      <td>XXX</td>
+      <td>XXX</td>
+      <td>Rp. 3.500.000</td>
+      <td width="130px"><button type="button" class="btn btn-outline-success" style="margin-right: 15px;"><i class="bi bi-pencil-square"></i></button>
+          <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+
+<div class="kaki">
+  <div class="container">
+    <div class="row">
+
+<div class="col-6">
+  <p>Showing 1-4 of 4 entries</p>
+</div>
+
+
+<div class="col-2">
+  <!--biarin kosong-->
+</div>
+
+
+<div class="col-4">
+<nav aria-label="...">
+  <ul class="pagination">
+    <li class="page-item disabled">
+      <a class="page-link">Previous</a>
+    </li>
+    <li class="page-item active" aria-current="page"><a class="page-link" >1</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">2</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Next</a>
+    </li>
+  </ul>
+</nav>
+</div>
+
+    </div>
+  </div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</body>
+</html><?php /**PATH D:\aplikasi\kodeno_finance\resources\views/Invoiceout.blade.php ENDPATH**/ ?>
